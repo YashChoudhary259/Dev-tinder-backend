4e39b12b765a3a9ebafd2df7e1fdab3d0b9bc5da
Yash <yashch26259@gmail.com>
2026-07-20 19:56:16 +0530

    create my frist express server

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const app = express();
+
+app.use("/test", (req,res) => {
+    res.send("this is a test route , what are you doing here?")
+})
+
+app.use("/hey", (req,res) => {
+    res.send("hey hey hey")
+})
+
+app.use("/hii", (req,res) => {
+    res.send("hii hii hii")
+})
+
+app.use((req,res) => {
+    res.send("hello from the server, how are you")
+})
+
+app.listen(3000, () => {
+    console.log("success")
+})
