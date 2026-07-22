875b75fd4ebaff5ce405e42c61f0a0a2a164a84d
Yash <yashch26259@gmail.com>
2026-07-22 11:51:14 +0530

    created login API

--- src/app.js
+++ src/app.js
@@ -2,24 +2,60 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+const { validateSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 app.use(express.json()); // for making data dynamic
 
 app.post("/signup", async (req, res) => {
-  //creating a new instance of my user model
-  // const user = new User({
-  //   firstName: "Sachin",
-  //   lastName: "Tendulkar",
-  //   emailId: "sachin@tendulakr.com",
-  //   password: "sachin@123",
-  // });
-  const user = new User(req.body);
-
   try {
+    // Validation of data
+    validateSignUpData(req);
+
+    const { firstName, lastName, emailId, password } = req.body;
+
+    // Encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    //creating a new instance of my user model
+    // const user = new User({
+    //   firstName: "Sachin",
+    //   lastName: "Tendulkar",
+    //   emailId: "sachin@tendulakr.com",
+    //   password: "sachin@123",
+    // });
+    const user = new User({
+      firstName,
+      lastName,
+      password: passwordHash,
+      emailId,
+    });
+
     await user.save();
     res.send("User Added Successfully");
   } catch (err) {
     res.status(400).send("Error saving the user: " + err.message);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid Credentials");
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (isPasswordValid) {
+      res.send("Login Successful");
+    } else {
+      throw new Error("Invalid Credentials");
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err.message);
   }
 });
 
@@ -61,10 +97,20 @@ app.delete("/user", async (req, res) => {
 });
 
 //Update data of the User
-app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
   const data = req.body;
+
   try {
+    const ALLOWED_UPDATES = ["photoUrl", "about", "skills"];
+
+    const isUpdatedAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k),
+    );
+
+    if (!isUpdatedAllowed) {
+      res.status(400).send("updates are not allowed");
+    }
     const user = await User.findByIdAndUpdate({ _id: userId }, data, {
       returnDocument: "after",
       runValidators: true,
--- src/models/user.js
+++ src/models/user.js
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    password: {
+      type: String,
+      required: true,
+    },
     age: {
       type: Number,
       min: 18,
